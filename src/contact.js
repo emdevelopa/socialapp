12d7018989c4da1b06576e9fb150ae057920dbd4
Your <gyimahemwurld@gmail.com>
2022-11-15 23:56:13 +0100

    derrr

--- src/contact.js
+++ src/contact.js
@@ -1,8 +1,15 @@
 import React from 'react'
 function Contact() {
     return (
-            <div>
-                deeddddddddddd
+            <div className='mainappcontainer contact-container'>
+            <form>
+                    <h2>CONTACT ME</h2>
+                    <input type='text' placeholder='First Name'/>
+                    <input type='text' placeholder='Last Name'/>
+                    <input type='text' placeholder='Email'/>
+                    <textarea placeholder='Measssge'></textarea>
+                    <input type='submit' />
+                </form>
             </div>
     )
 } 
